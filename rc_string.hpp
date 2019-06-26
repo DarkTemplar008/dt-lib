b08e2f104865e2c73315c07649a35fc05df0b826
shelei <shelei0601@qq.com>
2019-06-26 11:54:20 +0000

    增加共享字符串类

---
+++ rc_string.hpp
@@ -0,0 +1,92 @@
+#ifndef DT_BASE_RC_STRING_HPP
+#define DT_BASE_RC_STRING_HPP
+
+#include <memory>
+#include <string>
+
+namespace dt {
+namespace base {
+
+class String {
+public:
+	String() {
+		static std::shared_ptr<std::string> empty = std::make_shared<std::string>();
+		impl_ = empty;
+	}
+
+	template <class T>
+	String(T obj) {
+		impl_ = std::make_shared<std::string>(std::to_string(obj));
+	}
+
+	String(const char* str) {
+		impl_ = std::make_shared<std::string>(str);
+	}
+
+	String(const std::string& str) {
+		impl_ = std::make_shared<std::string>(str);
+	}
+
+	String(const String& other) {
+		impl_ = other.impl_;
+	}
+
+	String(String&& other) {
+		impl_ = other.impl_;
+		other.impl_.reset();
+	}
+
+	String& operator=(const String& other) {
+		impl_ = other.impl_;
+		return *this;
+	}
+
+	String& operator=(String&& other) {
+		impl_ = other.impl_;
+		other.impl_.reset();
+	}
+
+	String& operator=(const std::string& str) {
+		impl_ = std::make_shared<std::string>(str);
+		return *this;
+	}
+
+	operator std::string() const {
+		return *impl_;
+	}
+
+	bool operator == (const String& other) {
+		if (impl_ == other.impl_) {
+			return true;
+		} else if (*impl_ == *other.impl_) {
+			impl_ = other.impl_;
+			return true;
+		}
+		return false;
+	}
+
+	bool operator == (const String& other) const {
+		if (impl_ == other.impl_ || *impl_ == *other.impl_) {
+			return true;
+		}
+		return false;
+	}
+
+	bool operator == (const char* str) const {
+		return *impl_ == str;
+	}
+	bool operator == (const std::string& str) const {
+		return *impl_ == str;
+	}
+	bool operator < (const String& other) const {
+		return *impl_ < *other.impl_;
+	}
+
+private:
+	std::shared_ptr<std::string> impl_;
+};
+
+}
+}
+
+#endif
---
+++ test/rc_string_test.cpp
@@ -0,0 +1,39 @@
+#include <chrono>
+#include <iostream>
+
+#include "../rc_string.hpp"
+
+int main(int argc, char* argv[]) {
+	dt::base::String str[10];
+	for (int i = 0; i < 10; ++i) {
+		str[i] = dt::base::String(i);
+	}
+	std::string pstr[10];
+	for (int i = 0; i < 10; ++i) {
+		pstr[i] = str[i];
+	}
+
+	int compare_count = 100000000;
+
+	auto now = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < compare_count; ++i) {
+		if ( str[i%10] == str[(i+1)%10]) {
+			break;
+		}
+	}
+	std::cout << "begin:" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-now).count() << std::endl;
+
+	now = std::chrono::high_resolution_clock::now();
+	for (int i= 0; i < compare_count; ++i) {
+		if ( pstr[i%10] == pstr[(i+1)%10]) {
+			break;
+		}
+	}
+	std::cout << "end  :" << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-now).count() << std::endl;
+	return 0;
+}
+
+/*
+begin:4571
+end  :1947
+ */
